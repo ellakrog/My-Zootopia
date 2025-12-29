1a9cecd80fbcbf362e3259eab189dba74829fee0
Ljubica <krogella@gmail.com>
2025-12-29 11:08:12 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,26 +14,28 @@ def load_template(file_path):
 
 
 def generate_animals_info(animals_data):
-    """Generates string with animals data"""
+    """Generates HTML list items with animals data"""
     output = ""
 
     for animal in animals_data:
+        output += '<li class="cards__item">\n'
 
         if "name" in animal:
-            output += f"Name: {animal['name']}\n"
+            output += f"Name: {animal['name']}<br/>\n"
 
         if "characteristics" in animal and "diet" in animal["characteristics"]:
-            output += f"Diet: {animal['characteristics']['diet']}\n"
+            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
         if "locations" in animal and animal["locations"]:
-            output += f"Location: {animal['locations'][0]}\n"
+            output += f"Location: {animal['locations'][0]}<br/>\n"
 
         if "characteristics" in animal and "type" in animal["characteristics"]:
-            output += f"Type: {animal['characteristics']['type']}\n"
+            output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-        output += "\n"  # prazna linija između životinja
+        output += '</li>\n'
 
     return output
+
 
 
 def main():
